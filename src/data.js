e647e94179bb328506c35b8bb273c411d74b7fba
alelulu <ale.silvasuazo@gmail.com>
2019-04-23 13:00:55 -0400

    html showing data

--- src/data.js
+++ src/data.js
@@ -3,8 +3,16 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
+// acá va filtrar, cáculo y ordenar.
+
+//ordenando de A-Z
+const pkmnArray = POKEMON.pokemon;
+const orderByName = pkmnArray.sort(name)
+
+/*
 const example = () => {
   return 'example';
 };
 
 window.example = example;
+*/
--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 /* Manejo del DOM */
+//acá va cómo muestro la info (tabla)
+let pkmnArray = POKEMON.pokemon;
+let pkdxNumber = document.getElementById("pkdx-number");
+let aToZ = document.getElementById("a-z");
+let zToA = document.getElementById("z-a");
+aToZ.addEventListener("click" => (){
+
+})
+
+let pkmnBoxes = document.getElementById("boxes");
+let pkmnCount = 0
+
+for (let i = 0; i < pkmnArray.length; i++) {
+  let box = document.createElement("div");
+  box.classList.add("row")
+		if (pkmnCount < pkmnArray.length){
+			let pkmnContainer = document.createElement('div');
+      pkmnContainer.classList.add("pkmn-container", "col-sm-6");
+			pkmnContainer.innerHTML = "<h4 class='pkmn-name'>"+pkmnArray[pkmnCount].name+" #"+pkmnArray[pkmnCount].num+"</h4>";
+			pkmnContainer.innerHTML += "<br>"
+			pkmnContainer.innerHTML += "<img src='"+pkmnArray[pkmnCount].img+"'>"
+			pkmnContainer.innerHTML += "<p>"+pkmnArray[pkmnCount].type;+"</p>"
+      pkmnCount = pkmnCount + 1;
+  		box.appendChild(pkmnContainer);
+    }
+    pkmnBoxes.appendChild(box);
+	}
