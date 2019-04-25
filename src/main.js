9a7d3aa827d60da590bf39193bf98757f2f7c042
Carlar32 <carla.r.rivas2@gmail.com>
2019-04-25 14:39:05 -0400

    html structure and functions

--- src/main.js
+++ src/main.js
@@ -1,34 +1,64 @@
 /* Manejo del DOM */
-//acá va cómo muestro la info (tabla)
+//acá va cómo se muestra la info (tabla)
+//Declaramos variable para llamar array
 let pkmnArray = POKEMON.pokemon;
+//Mostrando en index todos los pkmn; llamamos a la funcion showPkmn con el load del body
+let indexPage = document.getElementById("index-page");
+if (indexPage != null){
+	window.addEventListener("load",() =>{
+		showPkmn(pkmnArray)
+	})
+}
+//Para que se resetee y vuelva a mostrar en orden por id. Usamos querySelectorAll para la clase reset. Llamamos a la funion showPkmn
+let pageReset = document.querySelectorAll(".reset");
+pageReset.forEach(element =>{
+	element.addEventListener("click", () => {
+		showPkmn(pkmnArray)
+	})
+})
+
+//yFunción para mostrar en cajitas responsivas los Pkmn y sus datos respectivos. Creamos div dinámico dentro de nuestro div del html ("boxes")
+let showPkmn = (arr) => {
+	let pkmnBoxes = document.getElementById("boxes");
+  pkmnBoxes.innerHTML = " ";
+  //Creamos un div para la clase row y lo alineamos (responsive)
+  let box = document.createElement("div");
+	box.classList.add("row");
+  box.classList.add("justify-content-center");
+  //Para que revise cada dato del array lo pasamos por un for. Dentro de este creamos un div con clases col y llamamos datos desde la data
+	for (let i = 0; i < arr.length; i++) {
+    let pkmnContainer = document.createElement('div');
+      pkmnContainer.classList.add("pkmn-container");
+			pkmnContainer.classList.add("col-sm-6");
+			pkmnContainer.classList.add("col-md-6");
+			pkmnContainer.classList.add("col-lg-3");
+			pkmnContainer.innerHTML = "<h4 class='pkmn-name'>"+arr[i].name+" #"+arr[i].num+"</h4>";
+			pkmnContainer.innerHTML += "<br>"
+			pkmnContainer.innerHTML += "<img src='"+arr[i].img+"'>"
+			pkmnContainer.innerHTML += "<p>"+arr[i].type;+"</p>"
+			box.appendChild(pkmnContainer);
+    }
+    pkmnBoxes.appendChild(box);
+}
+
+
+}
+
+// let pkmntype = [];
+// for (let i = 0; i < pkmnArray.length; i++); {
+// if (pkmnArray[pkmnCount].type === "Rock"){
+//  pkmntype.push(pkmnArray[pkmnCount].name);
+// }
+// }
+
+// console.log(pkmntype);
+
+const pkmntype = pkmnArray.filter(tipo => {
+		return tipo.type == "Rock";
+	})
+console.log(pkmntype);
+
 let pkdxNumber = document.getElementById("pkdx-number");
 let aToZ = document.getElementById("a-z");
 let zToA = document.getElementById("z-a");
 //aToZ.addEventListener("click", () => {
-
-//})
-
-let pkmnBoxes = document.getElementById("boxes");
-let pkmnCount = 0
-
-let box = document.createElement("div");
-box.classList.add("row");
-box.classList.add("justify-content-center");
-for (let i = 0; i < pkmnArray.length; i++) {
-  
-	if (pkmnCount < pkmnArray.length){
-		let pkmnContainer = document.createElement('div');
-		pkmnContainer.classList.add("pkmn-container");
-		pkmnContainer.classList.add("col-sm-12");
-		pkmnContainer.classList.add("col-md-6");
-		pkmnContainer.classList.add("col-lg-3");
-		pkmnContainer.innerHTML = "<h4 class='pkmn-name'>"+pkmnArray[pkmnCount].name+" #"+pkmnArray[pkmnCount].num+"</h4>";
-		pkmnContainer.innerHTML += "<br>"
-		pkmnContainer.innerHTML += "<img src='"+pkmnArray[pkmnCount].img+"'>"
-		pkmnContainer.innerHTML += "<p>"+pkmnArray[pkmnCount].type;+"</p>"
-		pkmnCount = pkmnCount + 1;
-		box.appendChild(pkmnContainer);
-	}
-}
-
-pkmnBoxes.appendChild(box);
