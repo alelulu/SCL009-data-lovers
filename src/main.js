34c3f91cb759978b60efa49feab886edbead6525
alelulu <ale.silvasuazo@gmail.com>
2019-05-15 19:07:20 -0400

    Fetch improved

--- src/main.js
+++ src/main.js
@@ -1,109 +1,55 @@
 // Calling the data
 //let pkmnArray = window.POKEMON.pokemon;
 
-fetch("https://raw.githubusercontent.com/alelulu/SCL009-data-lovers/master/src/data/pokemon/pokemon.json")
+//Calling JSON data
+window.addEventListener("load", () => {
+  fetch("https://raw.githubusercontent.com/alelulu/SCL009-data-lovers/master/src/data/pokemon/pokemon.json")
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
 		const pkmnArray = data.pokemon;
-    console.log(pkmnArray);
+    showPkmn(pkmnArray);
 
+    let pageReset = document.querySelectorAll(".reset");
+    pageReset.forEach(element =>{
+      element.addEventListener("click", () => {
+        window.currentArray = pkmnArray;
+        showPkmn(pkmnArray);
+      })
+    })
 
-			//Calling empty div to create cards
-			let pkmnBoxes = document.getElementById("boxes");
-			pkmnBoxes.innerHTML = " ";
-			let box = document.createElement("div");
-			box.classList.add("row");
-			box.classList.add("justify-content-center");
-			
-			for (let i = 0; i < pkmnArray.length; i++) {
-				//Creating cards
-				let pkmnContainer = document.createElement('div');
-				pkmnContainer.classList.add("card");
-				pkmnContainer.classList.add("side");
-				pkmnContainer.classList.add("pkmn-container");
-				pkmnContainer.classList.add("col-sm-6");
-				pkmnContainer.classList.add("col-md-6");
-				pkmnContainer.classList.add("col-lg-2");
-				pkmnContainer.classList.add("container");
-				//Calling names from data
-				let pkmnNameRow = document.createElement("div");
-				pkmnNameRow.classList.add("row");
-				let pkmnNameCol = document.createElement("div");
-				pkmnNameCol.classList.add("col");
-				pkmnNameCol.innerHTML = "<h6 class='pkmn-name'>"+pkmnArray[i].name+" #"+pkmnArray[i].num+"</h6>";
-				pkmnNameRow.appendChild(pkmnNameCol);
-				//Calling images from data
-				let pkmnImgRow = document.createElement("div");
-				pkmnImgRow.classList.add("row");
-				let pkmnImgCol = document.createElement("div");
-				pkmnImgCol.classList.add("col");
-				pkmnImgCol.innerHTML = "<img id='pkmn-img' src='"+pkmnArray[i].img+"'>"
-				pkmnImgRow.appendChild(pkmnImgCol)
-				//Calling types from data
-				let pkmnTypeRow = document.createElement("div");
-				pkmnTypeRow.classList.add("row");
-				let pkmnTypeCol = document.createElement("div");
-				//Creating conditions to add style style to pkmn with two types
-				if(pkmnArray[i].type[1] != undefined) {
-					let pkmnTypeCol1 = document.createElement("div");
-					pkmnTypeCol1.classList.add("col-3");
-					pkmnTypeCol1.classList.add("offset-3");
-					let pkmnTypeCol2 = document.createElement("div");
-					pkmnTypeCol2.classList.add("col-3");
-					pkmnTypeCol1.classList.add("types");
-					pkmnTypeCol2.classList.add("types");
-					let typeClass1 = pkmnArray[i].type[0].toLowerCase();
-					pkmnTypeCol1.innerHTML = "<p class=\"type-box "+typeClass1+"\" type-name=\""+pkmnArray[i].type[0]+"\">"+pkmnArray[i].type[0]+"</p>";
-					let typeClass2 = pkmnArray[i].type[1].toLowerCase();
-					pkmnTypeCol2.innerHTML = "<p class=\"type-box "+typeClass2+"\" type-name=\""+pkmnArray[i].type[1]+"\">"+pkmnArray[i].type[1]+"</p>";
-					pkmnTypeRow.appendChild(pkmnTypeCol1);
-					pkmnTypeRow.appendChild(pkmnTypeCol2);
-				}
-				//pkmn with only one type
-				else {
-					pkmnTypeCol = document.createElement("div");
-					pkmnTypeCol.classList.add("col-4");
-					pkmnTypeCol.classList.add("offset-4");
-					pkmnTypeCol.classList.add("types");
-					let typeClass = pkmnArray[i].type[0].toLowerCase();
-					pkmnTypeCol.innerHTML = "<p class=\"type-box "+typeClass+"\" type-name=\""+pkmnArray[i].type[0]+"\">"+pkmnArray[i].type[0]+"</p>";
-					pkmnTypeRow.appendChild(pkmnTypeCol);
-				}
-				pkmnContainer.appendChild(pkmnNameRow);
-				pkmnContainer.appendChild(pkmnImgRow);
-				pkmnContainer.appendChild(pkmnTypeRow);
-		
-				//Creating flip card
-				let backCard = document.createElement('div');
-				backCard.classList.add("side");
-				backCard.classList.add("back")
-				backCard.innerHTML = `<p> ⚖️ Peso: ${pkmnArray[i].weight}</p>
-				<p> ⬆️ Altura: ${pkmnArray[i].height}</p>
-				<p> 😧 Debilidades: </p>`;
-				//Creating list to show weaknesses
-				let weaknessList = document.createElement("ul");
-				weaknessList.innerHTML = " ";
-				pkmnArray[i].weaknesses.forEach(element => {
-					weaknessList.innerHTML += "<li class=\"weakness-list\">"+element+"</li>";
-				});
-				backCard.appendChild(weaknessList);
-		
-				pkmnContainer.appendChild(backCard);
-				box.appendChild(pkmnContainer);
-			}
-			pkmnBoxes.appendChild(box);
+    //Calling function to filter by type
+    let typeItems = document.querySelectorAll("button.type-item");
+    typeItems.forEach(element => {
+      element.addEventListener("click", () => {
+        showPkmn(window.data.filterType(pkmnArray, element.getAttribute("value")));
+      });
+    });
 
-let pageReset = document.querySelectorAll(".reset");
-pageReset.forEach(element =>{
-	element.addEventListener("click", () => {
-		window.currentArray = pkmnArray;
-		showPkmn(pkmnArray);
-	})
-})
+    //Calling function to filter by egg
+    let eggItems = document.querySelectorAll("button.egg-item");
+    eggItems.forEach(element => {
+      element.addEventListener("click", () => {
+        showPkmn(window.data.filterEgg(pkmnArray, element.getAttribute("value")))
+      })
+    })
 
-//Function to show cards
+    //Calling function to order the data
+    let orderedBy = document.querySelectorAll("button.ordered-by");
+    orderedBy.forEach(element => {
+      element.addEventListener("click", () => {
+        showPkmn(window.data.orderingBy(getCurrentArray(), element.getAttribute("value"), element.getAttribute("name")));
+      })
+    })
+
+    //Calling function to get our saved actual array
+    const getCurrentArray = () => {
+      return window.currentArray
+    }
+  });
+});
+
 let showPkmn = (arr) => {
 	//Calling function to calculate the total number of pkmn showed
 	let pkmnCount = document.getElementById("pkmn-count");
@@ -198,33 +144,3 @@ let showPkmn = (arr) => {
   }
   pkmnBoxes.appendChild(box);
 }
-
-//Calling function to filter by type
-let typeItems = document.querySelectorAll("button.type-item");
-typeItems.forEach(element => {
-	element.addEventListener("click", () => {
-		showPkmn(window.data.filterType(pkmnArray, element.getAttribute("value")));
-	});
-});
-
-//Calling function to filter by egg
-let eggItems = document.querySelectorAll("button.egg-item");
-eggItems.forEach(element => {
-	element.addEventListener("click", () => {
-		showPkmn(window.data.filterEgg(pkmnArray, element.getAttribute("value")))
-	})
-})
-
-//Calling function to order the data
-let orderedBy = document.querySelectorAll("button.ordered-by");
-orderedBy.forEach(element => {
-	element.addEventListener("click", () => {
-		showPkmn(window.data.orderingBy(getCurrentArray(), element.getAttribute("value"), element.getAttribute("name")));
-	})
-})
-
-//Calling function to get our saved actual array
-const getCurrentArray = () => {
-	return window.currentArray
-}
-});
